4ecdb9c7425f8622522fe0c6227e4a1cd6a86753
Gabriel Andrade Michalski <g.amichalski@gmail.com>
2020-04-18 18:35:05 +0000

    component navbar

--- buscador/src/App.vue
+++ buscador/src/App.vue
@@ -1,18 +1,31 @@
 <template>
   <div id="app">
+    <navbar>
+      <a slot="brand" class="navbar-item" href="https://bulma.io">
+        <img src="https://bulma.io/images/bulma-logo.png" alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28">
+      </a>
+      <a class="navbar-item" slot="start">
+        Home
+      </a>
+      <a class="navbar-item" slot="end">
+        Perfil
+      </a>
+    </navbar>
     <input class="input" placeholder="Digite sua pesquisa" v-model="filter"/>
     <list title="Buscas" :items="filteredItems"/>
   </div>
 </template>
 
 <script>
+import Navbar from "./components/Navbar";
 import List from "./components/List";
 import items from "./items"
 
 export default {
   name: "App",
   components: {
-    List
+    List,
+    Navbar
   },
   data() {
     return {
---
+++ buscador/src/components/Navbar.vue
@@ -0,0 +1,16 @@
+<template>
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <slot name="brand"/>
+  </div>
+  <div class="navbar-menu">
+    <div class="navbar-start">
+      <slot name="start"/>
+    </div>
+
+    <div class="navbar-end">
+      <slot name="end"/>
+    </div>  
+  </div>
+</nav>
+</template>
