20ddacf8a75d2743ba4c03a2e6ad1b2d25851648
Gabriel Andrade Michalski <g.amichalski@gmail.com>
2020-04-18 17:15:36 +0000

    component list base

--- buscador/src/App.vue
+++ buscador/src/App.vue
@@ -1,17 +1,16 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <list title="Buscas"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import List from "./components/List";
 
 export default {
   name: "App",
   components: {
-    HelloWorld
+    List
   }
 };
 </script>
---
+++ buscador/src/components/List.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+  <h2>{{title}}</h2>
+</div>
+</template>
+
+<script>
+export default {
+  props: {
+    title: String
+  },
+  data() {
+    return {
+    };
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
